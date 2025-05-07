969b18688946752c8f3b501f0a6016e466c4651a
ayebaishmo <ishmo255super@gmail.com>
2025-05-07 17:14:08 +0300

    vite and react setup

---
+++ admin/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Welcome  to buy nest</h1>
+      </div>
+      <div className="card">
+        <h2>Register new administrator</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
